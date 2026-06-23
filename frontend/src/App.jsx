2f5b730887d3d14684f9dd8a4c4cc18d2b893ca8
Swalehagameotech <zarekariswaleha@gmail.com>
2026-06-23 12:13:39 +0530

    tadaa

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -86,7 +86,7 @@ const App = () => {
         <Outlet />
       </main>
       {!isAdminRoute && (
-        <div className={isAuthPage ? "mt-6 sm:mt-8 lg:mt-10" : "mt-10 sm:mt-12 lg:mt-14"}>
+        <div className={isAuthPage ? "mt-6 sm:mt-8 lg:mt-10" : "mt-4 sm:mt-6 lg:mt-8"}>
           <Footer />
         </div>
       )}
